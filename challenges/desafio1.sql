c5bd282d7a9d4670689f9d02944e1997919acc0a
Shanloo <silmarfnascimento@gmail.com>
2023-06-06 19:49:12 -0300

    feat: implementa as tabelas e a configuração do desafio1.json

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -161,27 +161,3 @@ INSERT INTO SpotifyClone.History (user_id, song_id, play_date) VALUES
 	('8', '4', '"2012-03-17 14:56:41"'),
 	('9', '8', '"2022-02-24 21:14:22"'),
 	('10', '1', '"2015-12-13 08:30:22"');
-
--- Descomente e altere as linhas abaixo:
-
--- DROP DATABASE IF EXISTS SpotifyClone;
--- CREATE DATABASE IF NOT EXISTS SpotifyClone;
--- CREATE TABLE SpotifyClone.tabela1(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- CREATE TABLE SpotifyClone.tabela2(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
--- INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados X'),
---   ('exemplo de dados 2', 'exemplo de dados Y');
